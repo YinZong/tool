efa6ef3fd2489926e9812bed76f7dfbabd8d7f41
kevin_yz_chen <kevin_yz_chen@wistron.com>
2019-06-28 11:09:40 +0800

    create key

---
+++ create_api_key.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+def generate_key(length):
+    return ''.join(random.choice(string.ascii_letters + string.digits)\
+    	for _ in range(length))
+
+print(generate_key(16))
